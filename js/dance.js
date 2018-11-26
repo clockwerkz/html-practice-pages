aad6849b585c4edb0173e93a34ee96183fff23e2
Carlos Fins <clockwerkz@gmail.com>
2018-11-25 21:00:49 -0800

    Refactoring image slider code to be reusable

--- js/dance.js
+++ js/dance.js
@@ -1,74 +1,158 @@
-const slider = document.querySelector('.carousel__panels');
-let transX = -100;
-let objWidth =100;
-let upperLimit = -(100*4);
-let lowerLimit = 0;
-let reset=false;
-let setTransition = false;
-let timerObj=null;
-
-leftControl = document.querySelector('.carousel__control--left-control');
-rightControl = document.querySelector('.carousel__control--right-control');
-
-
-leftControl.addEventListener('click', ()=>{
-  if (timerObj) {
-    clearInterval(timerObj);
-    timerObj=null;
-  }
-  if (transX>= lowerLimit) return;
-  setAnimationTransition();
-  transX+=objWidth;
-  setTranslate(transX);
-  setSlideTimer();
-});
-
-rightControl.addEventListener('click', ()=>{
-  if (timerObj) {
-    clearInterval(timerObj);
-    timerObj=null;
-  }
-  if (transX<=upperLimit) return;
-  setAnimationTransition();
-  transX-=objWidth;
-  setTranslate(transX);
-  setSlideTimer();
-});
-
-
-slider.addEventListener('transitionend', ()=> {
-  if (transX<=upperLimit) {
-    setAnimationTransition(false);
-    transX=lowerLimit-objWidth;
-    setTranslate(transX);
-  } else if (transX>= lowerLimit) {
-    setAnimationTransition(false);
-    transX=upperLimit+100;
-    setTranslate(transX);
-  }
-});
-
-const setSlideTimer = () => {
-  if (!timerObj) {
-    timerObj = setInterval(()=> {
-      setAnimationTransition();
-      transX-=objWidth;
-      setTranslate(transX);
-    }, 6000);
-  }
+const heroSliderSetup = {
+  dimension : 100,
+  carouselObj : document.querySelector(".hero")
 }
 
-const setTranslate = (value) => {
-    slider.style.transform = `translateX(${value}%)`;
-}
+createSlider = ( { dimension, carouselObj } ) => {
+  const slider = carouselObj.querySelector('.carousel__panels');
+  let transX = -(dimension);
+  let objWidth = dimension;
+  let upperLimit = -(dimension*4);
+  let lowerLimit = 0;
+  let reset=false;
+  let setTransition = false;
+  let timerObj=null;
 
-const setAnimationTransition = (animated=true) => {
-    if (animated) {
-        slider.style.transition = 'transform 0.75s ease-in-out';
-    } else {
-        slider.style.transition = 'none'; 
+  let leftControl = carouselObj.querySelector('.carousel__control--left-control');
+  let rightControl = carouselObj.querySelector('.carousel__control--right-control');
+
+  leftControl.addEventListener('click', ()=>{
+    if (timerObj) {
+      clearInterval(timerObj);
+      timerObj=null;
     }
+    if (transX>= lowerLimit) return;
+    setAnimationTransition();
+    transX+=objWidth;
+    setTranslate(transX);
+    setSlideTimer();
+  });
+  
+  rightControl.addEventListener('click', ()=>{
+    if (timerObj) {
+      clearInterval(timerObj);
+      timerObj=null;
+    }
+    if (transX<=upperLimit) return;
+    setAnimationTransition();
+    transX-=objWidth;
+    setTranslate(transX);
+    setSlideTimer();
+  });
+  
+  
+  slider.addEventListener('transitionend', ()=> {
+    if (transX<=upperLimit) {
+      setAnimationTransition(false);
+      transX=lowerLimit-objWidth;
+      setTranslate(transX);
+    } else if (transX>= lowerLimit) {
+      setAnimationTransition(false);
+      transX=upperLimit+100;
+      setTranslate(transX);
+    }
+  });
+  
+  const setSlideTimer = () => {
+    if (!timerObj) {
+      timerObj = setInterval(()=> {
+        setAnimationTransition();
+        transX-=objWidth;
+        setTranslate(transX);
+      }, 6000);
+    }
+  }
+  
+  const setTranslate = (value) => {
+      slider.style.transform = `translateX(${value}%)`;
+  }
+  
+  const setAnimationTransition = (animated=true) => {
+      if (animated) {
+          slider.style.transition = 'transform 0.75s ease-in-out';
+      } else {
+          slider.style.transition = 'none'; 
+      }
+  }
+  
+  
+  setSlideTimer();
 }
 
+createSlider(heroSliderSetup);
 
-setSlideTimer();
+
+// const slider = document.querySelector('.carousel__panels');
+// let transX = -100;
+// let objWidth =100;
+// let upperLimit = -(100*4);
+// let lowerLimit = 0;
+// let reset=false;
+// let setTransition = false;
+// let timerObj=null;
+
+// leftControl = document.querySelector('.carousel__control--left-control');
+// rightControl = document.querySelector('.carousel__control--right-control');
+
+
+// leftControl.addEventListener('click', ()=>{
+//   if (timerObj) {
+//     clearInterval(timerObj);
+//     timerObj=null;
+//   }
+//   if (transX>= lowerLimit) return;
+//   setAnimationTransition();
+//   transX+=objWidth;
+//   setTranslate(transX);
+//   setSlideTimer();
+// });
+
+// rightControl.addEventListener('click', ()=>{
+//   if (timerObj) {
+//     clearInterval(timerObj);
+//     timerObj=null;
+//   }
+//   if (transX<=upperLimit) return;
+//   setAnimationTransition();
+//   transX-=objWidth;
+//   setTranslate(transX);
+//   setSlideTimer();
+// });
+
+
+// slider.addEventListener('transitionend', ()=> {
+//   if (transX<=upperLimit) {
+//     setAnimationTransition(false);
+//     transX=lowerLimit-objWidth;
+//     setTranslate(transX);
+//   } else if (transX>= lowerLimit) {
+//     setAnimationTransition(false);
+//     transX=upperLimit+100;
+//     setTranslate(transX);
+//   }
+// });
+
+// const setSlideTimer = () => {
+//   if (!timerObj) {
+//     timerObj = setInterval(()=> {
+//       setAnimationTransition();
+//       transX-=objWidth;
+//       setTranslate(transX);
+//     }, 6000);
+//   }
+// }
+
+// const setTranslate = (value) => {
+//     slider.style.transform = `translateX(${value}%)`;
+// }
+
+// const setAnimationTransition = (animated=true) => {
+//     if (animated) {
+//         slider.style.transition = 'transform 0.75s ease-in-out';
+//     } else {
+//         slider.style.transition = 'none'; 
+//     }
+// }
+
+
+// setSlideTimer();
